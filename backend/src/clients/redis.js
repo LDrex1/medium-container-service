64eade4a076282ce89cbf1cb06a89b5b2e820862
Ldrex1 <damzealwini7@gmail.com>
2025-07-05 10:51:13 +0100

    update: redis port and host defination

--- backend/src/clients/redis.js
+++ backend/src/clients/redis.js
@@ -1,4 +1,7 @@
 const Redis = require("ioredis");
-const redis = new Redis();
+const redis = new Redis({
+  host: process.env.REDIS_HOST || "redis",
+  port: process.env.REDIS_PORT6379 || 6379,
+});
 
 export default redis;
