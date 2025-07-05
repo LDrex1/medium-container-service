ca68d4b16120154005172aab9358d645bdf02028
Ldrex1 <damzealwini7@gmail.com>
2025-07-05 13:46:32 +0100

    update: using env instead of hard coded values

--- backend/src/clients/db.js
+++ backend/src/clients/db.js
@@ -1,10 +1,10 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 mongoose
-  .connect(process.env.MONGO_URI, {
+  .connect(process.env.MONGO_URI || "", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
   })
-  .then(() => console.log('MongoDB: Connectted'))
+  .then(() => console.log("MongoDB: Connected Succesfully"))
   .catch((err) => console.log(err.message));
